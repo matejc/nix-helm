e0da40d7c60d1a0219485a956ddbb29b2138f371
Gytis Ivaskevicius <me@gytis.io>
2023-01-31 17:56:54 +0200

    fix: Values path

--- src/default.nix
+++ src/default.nix
@@ -15,9 +15,9 @@ let
     in
     {
       inherit name output values;
-      install = mkHelmCommand "install" ''--values "${output}" "${output}"'';
+      install = mkHelmCommand "install" ''--values "${output}/values.yaml" "${output}"'';
       status = mkHelmCommand "status" "";
-      upgrade = mkHelmCommand "upgrade" ''--values "${output}" "${output}"'';
+      upgrade = mkHelmCommand "upgrade" ''--values "${output}/values.yaml" "${output}"'';
       uninstall = mkHelmCommand "uninstall" "";
     };
 
